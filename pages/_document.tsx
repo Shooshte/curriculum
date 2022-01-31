5a77bb62c1127381642fa9c752f708fbb54e5001
Miha Sustersic <miha.sustersic.work@gmail.com>
2022-01-31 16:39:11 +0100

    added crossorigin to the google fonts link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,10 @@ const Document = () => {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" />
+        {/* 
+  // @ts-ignore */}
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&display=swap"
           rel="stylesheet"
