f55acf72b99a8fdfdc19d443f98bdca48a948316
Miha Sustersic <miha.sustersic.work@gmail.com>
2022-02-17 08:43:44 +0100

    google site verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ const Document = () => {
       <Head>
         <meta
           name="google-site-verification"
-          content="fqQbZsKvnSMxXQBpgDJE3hO_nS5ObNIDZquyED3n6OM"
+          content="azLX9xdN1I3il26NwhmTxWGi5F9iYsKJobI6ZyRZRXg"
         />
         <link rel="preconnect" href="https://fonts.gstatic.com/" />
         <link rel="preconnect" href="https://fonts.gstatic.com/" />
