aec4611d2a50d0ff516a0391c34546626d8eb9e4
shooshte <miha.sustersic.work@gmail.com>
2022-03-02 18:40:38 +0100

    redesign blog posts list page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,3 +1,5 @@
+import shave from "shave";
+import { useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
@@ -23,51 +25,94 @@ const parseCategories = (categoriesString: string): string[] => {
   return categoriesString.split(";");
 };
 
+const useWindowSize = () => {
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Truncate blog post title
+      shave("#blogTitle", 57, { character: "...", spaces: false });
+      shave("#description", 54, { character: "...", spaces: false });
+      shave("#categories", 14, { character: "...", spaces: false });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+};
+
 const Blog = ({ allPostsData }: PropsType) => {
+  useWindowSize();
+
   return (
     <>
       <Head>
         <title>Miha Šušteršič: Blog</title>
         <meta
           name="description"
-          content="Miha Šušteršič's personal blog page. Focused on ReactJS and front-end development but may also include other topics and cat pictures."
+          content="Miha Šušteršič's personal blog page. Focused on ReactJS, front-end, and software development but may also include other topics and cat pictures."
         ></meta>
       </Head>
-      <section className={`content-container ${styles.container}`}>
-        <h2 className="heading-2">Recent blog posts</h2>
-        <ul>
-          {allPostsData.map(
-            ({ categories, description, id, imageUrl, title }) => {
-              const categoriesArray = parseCategories(categories);
+      <section className={styles.container}>
+        <div>
+          <h2 className={`${styles.title} heading-2`}>Recent blog posts</h2>
+          <ul>
+            {allPostsData.map(
+              ({ categories, date, description, id, imageUrl, title }) => {
+                const dateText = new Date(date).toLocaleDateString("en-US", {
+                  day: "numeric",
+                  month: "short",
+                });
+                const categoriesArray = parseCategories(categories);
+                const categoriesText = categoriesArray.reduce(
+                  (acc, category, index) => {
+                    return index > 0
+                      ? `${acc} - ${category}`
+                      : `${acc}  |  ${category}`;
+                  },
+                  dateText
+                );
 
-              return (
-                <li className={styles.blogItem} key={id}>
-                  {imageUrl ? (
-                    <div className={styles.imageContainer}>
-                      <Image
-                        alt={`blog post header`}
-                        layout="fill"
-                        objectFit="cover"
-                        src={imageUrl}
-                      />
+                return (
+                  <li className={styles.blogItem} key={id}>
+                    <div className={styles.blogText}>
+                      <Link href={`blog/${id}`} passHref={false}>
+                        <h3
+                          className={`heading-4 link ${styles.blogTitle}`}
+                          id="blogTitle"
+                        >
+                          {title}
+                        </h3>
+                      </Link>
+                      <p
+                        className={`text ${styles.description}`}
+                        id="description"
+                      >
+                        {description}
+                      </p>
+                      <p className={styles.categories} id="categories">
+                        {categoriesText}
+                      </p>
                     </div>
-                  ) : null}
-                  <Link href={`blog/${id}`} passHref={false}>
-                    <h3 className="heading-3 link">{title}</h3>
-                  </Link>
-                  <div className={styles.categories}>
-                    {categoriesArray.map((category, index) => (
-                      <div key={`${category}-${index}`} className="pill">
-                        {category}
+                    {imageUrl ? (
+                      <div className={styles.imageContainer}>
+                        <Image
+                          alt={`blog post header`}
+                          layout="fill"
+                          objectFit="cover"
+                          src={imageUrl}
+                        />
                       </div>
-                    ))}
-                  </div>
-                  <p className="text">{description}</p>
-                </li>
-              );
-            }
-          )}
-        </ul>
+                    ) : null}
+                  </li>
+                );
+              }
+            )}
+          </ul>
+        </div>
+        <div className={styles.accent} />
       </section>
     </>
   );
