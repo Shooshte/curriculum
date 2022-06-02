6c7e99aad3a14dcd457dd3eaaffdbd4a6daadeea
Miha Šušteršič <miha.sustersic.work@gmail.com>
2022-06-02 17:41:32 +0200

    Feature/sentry (#42)
    * added withSentry back, moved neo4j driver initialisation to the handler function

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,31 +5,33 @@ import GoogleProvider from "next-auth/providers/google";
 
 // import { withSentry } from "@sentry/nextjs";
 
-const driver = neo4j.driver(
-  process.env.NEO4J_URI,
-  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASS)
-);
+const handler = () => {
+  const driver = neo4j.driver(
+    process.env.NEO4J_URI,
+    neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASS)
+  );
 
-const neo4jSession = driver.session();
+  const neo4jSession = driver.session();
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
+  return NextAuth({
+    providers: [
+      GoogleProvider({
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+          },
         },
-      },
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  adapter: Neo4jAdapter(neo4jSession),
-  session: {
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-});
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+    ],
+    adapter: Neo4jAdapter(neo4jSession),
+    session: {
+      maxAge: 30 * 24 * 60 * 60, // 30 days
+    },
+  });
+};
 
 export default handler;
